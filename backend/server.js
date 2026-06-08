6ed977ec3613d436942503c4b55360e8d40fe4f2
TomasTolo <tolotomas04@outlook.com>
2026-06-08 23:38:32 +0200

    Update server.js
    pakat ihop data och lägt till endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,58 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+// hämta från seed
+const Room = require('./models/Room');
+const Sensor = require('./models/Sensor');
 
 const app = express();
 
-app.use(cors());          
-app.use(express.json());  
-
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(' ansluten till atlas'))
-  .catch(err => console.error('gick inte att ansluta ', err));
+  .then(() => console.log('ansluten till mongodb atlas'))
+  .catch(err => console.error('Kunde inte ansluta till MongoDB:', err));
 
-
+// Bas-URL för att testa att servern lever
 app.get('/', (req, res) => {
-  res.send('servern fungerar');
+  res.send('Smart Home API funkar');
 });
+
+
+
+app.get('/api/rooms', async (req, res) => {
+  try {
+    // Hämta alla rum och alla sensorer från Atlas samtidigt
+    const rooms = await Room.find();
+    const sensors = await Sensor.find();
+
+   
+    const completeData = rooms.map(room => {
+      
+      // Hitta sensor för rummet med room ID 
+      const roomSensors = sensors.filter(sensor => sensor.roomId.toString() === room._id.toString());
+      
+      // kompinera rumsdata  
+      return {
+        _id: room._id,
+        name: room.name,
+        floor: room.floor,
+        sensors: roomSensors 
+      };
+    });
+
+    // skicka data till frontend 
+    res.json(completeData);
+
+  } catch (error) {
+    console.error("Fel vid hämtning av data:", error);
+    res.status(500).json({ message: "Serverfel vid hämtning av rum" });
+  }
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servern har startat och körs på http://localhost:${PORT}`);
+  console.log(`Servern körs på http://localhost:${PORT}`);
 });
