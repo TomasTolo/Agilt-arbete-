cb1f9e6343f7ccf289b19fe06d91c41b5575f5c1
TomasTolo <tolotomas04@outlook.com>
2026-06-08 14:50:20 +0200

    starta server koppla med atlas

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("Starting server");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(cors());          
+app.use(express.json());  
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log(' ansluten till atlas'))
+  .catch(err => console.error('gick inte att ansluta ', err));
+
+
+app.get('/', (req, res) => {
+  res.send('servern fungerar');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servern har startat och körs på http://localhost:${PORT}`);
+});
